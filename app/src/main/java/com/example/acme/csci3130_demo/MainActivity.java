9b991e74b5a8f7c88d584f6cee4c84799e27a5cd
Michael <1525940982@qq.com>
2018-06-03 12:17:36 -0300

    Nothing to talk

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout.activity_main);
         textView=findViewById(R.id.textView);
         editText=findViewById(R.id.editText);
         button=(Button) findViewById(R.id.button);
